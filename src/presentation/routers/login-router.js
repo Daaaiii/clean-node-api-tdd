6ac837ee40e2d86a3d9da50f7548059a25ea2de2
Daiane Bolzan <dainha85@gmail.com>
2024-01-20 19:53:49 -0300

    modify HttpResponse.badrequest to receive an error"

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,4 +1,5 @@
 const HttpResponse = require('../helpers/http-response')
+const MissingParamError = require('../helpers/missing-params-erros')
 module.exports = class LoginRouter {
   constructor (authUseCase) {
     this.authUseCase = authUseCase
@@ -9,11 +10,14 @@ module.exports = class LoginRouter {
       const { email, password } = httpRequest.body
 
       if (!email) {
-        return HttpResponse.badRequest('email')
+        return HttpResponse.badRequest(new MissingParamError('email'))
       }
+      // if (!/email/.test(email)) {
+      //   return HttpResponse.badRequest('email')
+      // }
 
       if (!password) {
-        return HttpResponse.badRequest('password')
+        return HttpResponse.badRequest(new MissingParamError('password'))
       }
 
       const accessToken = await this.authUseCase.auth(email, password)
