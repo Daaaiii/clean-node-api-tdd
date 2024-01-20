cca01da20ecb8080384cb8a2b590fd8c64a7ca4c
Daiane Bolzan <dainha85@gmail.com>
2024-01-20 13:14:44 -0300

    move production code to their own files

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,9 +1,16 @@
+const HttpResponse = require('../helpers/http-response')
 module.exports = class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400
-      }
+    if (!httpRequest || !httpRequest.body) {
+      return HttpResponse.serverError()
+    }
+    const { email, password } = httpRequest.body
+
+    if (!email) {
+      return HttpResponse.badRequest('email')
+    }
+    if (!password) {
+      return HttpResponse.badRequest('password')
     }
   }
 }
